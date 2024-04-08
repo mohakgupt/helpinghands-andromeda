031119d43b44097a3667ee041fdfa45978370232
mohakgupt <thisismohak@gmail.com>
2024-04-08 21:17:37 +0530

    removed files

--- src/lib/app/types.ts
+++ src/lib/app/types.ts
@@ -1,59 +1,52 @@
 export interface IConfig extends IShareUrls {
-    name: string;
-    chainId: string;
-    coinDenom: string;
-    collections: ICollection[];
-    id: string;
-    createdDate: string;
-    modifiedDate: string;
-    banner?: string;
+  name: string;
+  chainId: string;
+  coinDenom: string;
+  collections: ICollection[];
+  id: string;
+  createdDate: string;
+  modifiedDate: string;
+  banner?: string;
 }
 
 export interface IBaseCollection extends IShareUrls {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
 export enum ICollectionType {
-    AUCTION = 'embeddables-auction',
-    MARKETPLACE = 'embeddables-marketplace',
-    CROWDFUND = 'embeddables-crowdfund',
-    EXCHANGE = 'embeddables-exchange',
+  AUCTION = "embeddables-auction",
+  MARKETPLACE = "embeddables-marketplace",
+  CROWDFUND = "embeddables-crowdfund",
 }
 
-
 export interface IAuctionCollection extends IBaseCollection {
-    auction: string;
-    cw721: string;
-    featured?: string;
-    type: ICollectionType.AUCTION;
+  auction: string;
+  cw721: string;
+  featured?: string;
+  type: ICollectionType.AUCTION;
 }
 
 export interface IMarketplaceCollection extends IBaseCollection {
-    marketplace: string;
-    cw721: string;
-    featured?: string;
-    type: ICollectionType.MARKETPLACE;
+  marketplace: string;
+  cw721: string;
+  featured?: string;
+  type: ICollectionType.MARKETPLACE;
 }
 
 export interface ICrowdfundCollection extends IBaseCollection {
-    crowdfund: string;
-    cw721: string;
-    featured?: string;
-    type: ICollectionType.CROWDFUND;
+  crowdfund: string;
+  cw721: string;
+  featured?: string;
+  type: ICollectionType.CROWDFUND;
 }
 
-export interface IExchangeCollection extends IBaseCollection {
-    exchange: string;
-    cw20: string;
-    type: ICollectionType.EXCHANGE;
-}
-
-export type ICollectionCw20 = IExchangeCollection;
-export type ICollectionCw721 = IAuctionCollection | IMarketplaceCollection | ICrowdfundCollection;
-export type ICollection = ICollectionCw20 | ICollectionCw721;
-
+export type ICollectionCw721 =
+  | IAuctionCollection
+  | IMarketplaceCollection
+  | ICrowdfundCollection;
+export type ICollection = ICollectionCw721;
 
 export interface IShareUrls {
-    twitter?: string;
+  twitter?: string;
 }
